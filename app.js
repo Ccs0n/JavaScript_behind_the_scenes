21f34e1a5072196a9b711e1ae5a1cfb060df15c8
Menelaos Gkotsopoulos <menelaosgkotsopoulos@gmail.com>
2025-01-11 00:30:39 +0200

    just soem theory about block scope

---
+++ app.js
@@ -0,0 +1,14 @@
+// var name = 'Menelaos';
+let name = 'Menelaos';
+
+if (name === 'Menelaos') {
+    let hobbies = ['Mugai Ryu', 'running']
+    console.log(hobbies);
+}
+function greet() {
+    let age = 35;
+    let name = 'Manolis';
+    console.log(name, age, hobbies);
+}
+console.log(name, hobbies);
+greet();
